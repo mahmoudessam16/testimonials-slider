e903b9193833a356486a52fc7331b427084e5949
mahmoudessam16 <mahmoudemourad29@gmail.com>
2024-02-14 15:51:55 +0300

    modify route of images

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ let tests = [
         name: "Tanya Sinclair",
         job: "UX Engineer",
         para: "“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”",
-        img: "http://127.0.0.1:5500/images/image-tanya.jpg"
+        img: "/images/image-tanya.jpg"
     },
     {
         name: "John Tarkpor",
         job: "Junior Front-end Developer",
         para: "“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
-        img: "http://127.0.0.1:5500/images/image-john.jpg",
+        img: "/images/image-john.jpg",
     }
 ];
 
